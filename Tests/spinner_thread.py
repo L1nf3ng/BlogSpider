bb7174936ef14fb5cb2379261cfec0a07ceafe71
L1nf3ng <nuaa_llf@126.com>
2019-05-22 14:59:40 +0800

    py版的netcat，首先实现的功能是'-lvnp'

---
+++ Tests/spinner_thread.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+"""
+@author: d00ms(--)
+@file: spinner_thread.py
+@time: 2019-5-22 13:55
+@desc:
+"""
+
+import threading
+import itertools
+import time
+import sys
+import unittest
+
+
+class Signal:
+    go = True
+
+def spin(msg, signal):
+    write, flush = sys.stdout.write, sys.stdout.flush
+    for char in itertools.cycle('|/-\\'):
+        status = char + ' ' + msg
+        write(status)
+        flush()
+        write('\x08' * len(status))
+        time.sleep(.1)
+        if not signal.go:
+            break
+    write(' ' * len(status) + '\x08' * len(status))
+
+
+def slow_function():
+    # 假装等待I/O一段时间
+    time.sleep(3)
+    return 42
+
+
+def supervisor():
+    signal = Signal()
+    spinner = threading.Thread(target=spin,
+    args=('thinking!', signal))
+    print('spinner object:', spinner)
+    spinner.start()
+    result = slow_function()
+    signal.go = False
+    spinner.join()
+    return result
+
+def main():
+    result = supervisor()
+    print('Answer:', result)
+
+class Tester(unittest.TestCase):
+    def testMain(self):
+        main()
+
+if __name__ == '__main__':
+    unittest.main()
+
--- pyNC.py
+++ pyNC.py
@@ -7,3 +7,59 @@
 @time: 2019-5-22 10:21
 @desc: 公司杀软居然杀netcat，天理何在，决定空闲时间写一写py版的nc
 """
+
+import sys
+import getopt
+import socket
+
+def Usage():
+    usage='''usage: nc [-46CDdFhklNnrStUuvZz] [-I length] [-i interval] [-M ttl]
+	  [-m minttl] [-O length] [-P proxy_username] [-p source_port]
+	  [-q seconds] [-s source] [-T keyword] [-V rtable] [-W recvlimit] [-w timeout]
+	  [-X proxy_protocol] [-x proxy_address[:port]] 	  [destination] [port]
+	Command Summary:
+		-4		Use IPv4
+		-6		Use IPv6
+		-b		Allow broadcast
+		-C		Send CRLF as line-ending
+		-D		Enable the debug socket option
+		-d		Detach from stdin
+		-F		Pass socket fd
+		-h		This help text
+		-I length	TCP receive buffer length
+		-i interval	Delay interval for lines sent, ports scanned
+		-k		Keep inbound sockets open for multiple connects
+		-l		Listen mode, for inbound connects
+		-M ttl		Outgoing TTL / Hop Limit
+		-m minttl	Minimum incoming TTL / Hop Limit
+		-N		Shutdown the network socket after EOF on stdin
+		-n		Suppress name/port resolutions
+		-O length	TCP send buffer length
+		-P proxyuser	Username for proxy authentication
+		-p port		Specify local port for remote connects
+		-q secs		quit after EOF on stdin and delay of secs
+		-r		Randomize remote ports
+		-S		Enable the TCP MD5 signature option
+		-s source	Local source address
+		-T keyword	TOS value
+		-t		Answer TELNET negotiation
+		-U		Use UNIX domain socket
+		-u		UDP mode
+		-V rtable	Specify alternate routing table
+		-v		Verbose
+		-W recvlimit	Terminate after receiving a number of packets
+		-w timeout	Timeout for connects and final net reads
+		-X proto	Proxy protocol: "4", "5" (SOCKS) or "connect"
+		-x addr[:port]	Specify proxy address and port
+		-Z		DCCP mode
+		-z		Zero-I/O mode [used for scanning]
+	Port numbers can be individual or ranges: lo-hi [inclusive]
+	'''
+    print(usage)
+
+if __name__ == '__main__':
+    Usage()
+
+
+
+
